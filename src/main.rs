ef0bf8e4e1957f050c2a7a1b06231d5e21174c49
jaskij <jaskij@gmail.com>
2024-12-23 00:22:53 +0100

    make the example more minimal, fix lints

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-use std::fs::File;
-use std::io::BufWriter;
-use std::ptr;
-use std::time::Duration;
 use anyhow::Result;
+use log::info;
 use opencl3::command_queue::{CommandQueue, CL_QUEUE_PROFILING_ENABLE};
 use opencl3::context::Context;
 use opencl3::device::{get_all_devices, Device, CL_DEVICE_TYPE_GPU};
@@ -10,11 +7,14 @@ use opencl3::event::cl_event;
 use opencl3::kernel::{ExecuteKernel, Kernel};
 use opencl3::memory::{Buffer, CL_MEM_WRITE_ONLY};
 use opencl3::program::Program;
-use opencl3::types::{cl_float, CL_BLOCKING, CL_NON_BLOCKING};
-use tracing::info;
+use opencl3::types::CL_NON_BLOCKING;
+use std::fs::File;
+use std::io::BufWriter;
+use std::ptr;
+use std::time::Duration;
 
 const KERNEL_SOURCE: &str = include_str!("test.cl");
-const KERNEL_NAME: &str = "render_kernel";
+const KERNEL_NAME: &str = "mandelbrot";
 
 const IMAGE_WIDTH: usize = 640;
 const IMAGE_HEIGHT: usize = 480;
@@ -22,21 +22,13 @@ const IMAGE_SIZE: usize = IMAGE_WIDTH * IMAGE_HEIGHT;
 const IMAGE_ARRAY_SIZE: usize = IMAGE_SIZE * 3;
 
 fn main() -> Result<()> {
-    tracing_subscriber::fmt()
-        .with_env_filter("INFO")
-        .with_writer(std::io::stdout)
-        .compact()
-        .init();
+    env_logger::init();
 
     let device_id = *get_all_devices(CL_DEVICE_TYPE_GPU)?
         .first()
         .expect("no devices found");
     let device = Device::new(device_id);
-    info!(
-        name = device.name()?,
-        vendor = device.vendor()?,
-        "running on"
-    );
+    info!("running on {} {} ", device.vendor()?, device.name()?);
 
     let runtime_start = std::time::Instant::now();
 
@@ -47,7 +39,7 @@ fn main() -> Result<()> {
 
     let queue = CommandQueue::create_default(&context, CL_QUEUE_PROFILING_ENABLE)?;
 
-    let mut buffer = unsafe {
+    let buffer = unsafe {
         Buffer::<u8>::create(
             &context,
             CL_MEM_WRITE_ONLY,
@@ -59,21 +51,20 @@ fn main() -> Result<()> {
     let kernel_event = unsafe {
         ExecuteKernel::new(&kernel)
             .set_arg(&buffer)
-            .set_global_work_size(IMAGE_SIZE)
+            .set_global_work_sizes(&[IMAGE_WIDTH, IMAGE_HEIGHT])
             .enqueue_nd_range(&queue)?
     };
-    let mut events: Vec<cl_event> = vec![kernel_event.get()];
+    let events: Vec<cl_event> = vec![kernel_event.get()];
 
-    let mut results: [u8; IMAGE_ARRAY_SIZE] = [0; IMAGE_ARRAY_SIZE];
+    let mut results = vec![0; IMAGE_ARRAY_SIZE];
     let read_event =
         unsafe { queue.enqueue_read_buffer(&buffer, CL_NON_BLOCKING, 0, &mut results, &events)? };
     read_event.wait()?;
 
     let run_duration = runtime_start.elapsed();
-    info!(?run_duration);
 
     let file = File::create("out.png")?;
-    let mut writer = BufWriter::new(file);
+    let writer = BufWriter::new(file);
     let mut encoder = png::Encoder::new(writer, IMAGE_WIDTH as u32, IMAGE_HEIGHT as u32);
     encoder.set_color(png::ColorType::Rgb);
     encoder.set_depth(png::BitDepth::Eight);
@@ -84,8 +75,11 @@ fn main() -> Result<()> {
     let start_time = kernel_event.profiling_command_start()?;
     let end_time = kernel_event.profiling_command_end()?;
     let kernel_runtime = Duration::from_nanos(end_time - start_time);
-    info!(?kernel_runtime);
 
+    info!(
+        "program took {:?}, of which kernel took {:?}",
+        run_duration, kernel_runtime
+    );
 
     Ok(())
 }
--- src/test.cl
+++ src/test.cl
@@ -1,11 +1,57 @@
-void kernel render_kernel(global uchar * rgba_out)
-{
-    const float3 final_colour_linear = (float3)(0.3f, 0.1f, 0.7f);
+// Used to index into the 1D array, so that we can use
+// it effectively as a 2D array
+int index(int x, int y, int width) {
+  return 3*width*y + x*3;
+}
 
-    // from http://chilliant.blogspot.jp/2012/08/srgb-approximations-for-hlsl.html
-    const float3 final_colour_sRGB = min(255, max(0.0f, 1.055f * pow(final_colour_linear, 0.416666667f) - 0.055f) * 255);
+// Turn the raw x coordinates [0, 1] into a scaled x coordinate
+// [0, 1] -> [-2, 1.25]
+float mapX(float x) {
+  return x*3.25 - 2;
+}
 
-    rgba_out[get_global_id(0) * 3 + 0] = (uchar)(final_colour_sRGB.x);
-    rgba_out[get_global_id(0) * 3 + 1] = (uchar)(final_colour_sRGB.y);
-    rgba_out[get_global_id(0) * 3 + 2] = (uchar)(final_colour_sRGB.z);
+// Same purpose as mapX
+// [0, 1] -> [-1.25, 1.25]
+float mapY(float y) {
+  return y*2.5 - 1.25;
+}
+
+void kernel mandelbrot(global uchar *out) {
+  int x_dim = get_global_id(0);
+  int y_dim = get_global_id(1);
+  size_t width = get_global_size(0);
+  size_t height = get_global_size(1);
+  int idx = index(x_dim, y_dim, width);
+
+  float x_origin = mapX((float) x_dim / width);
+  float y_origin = mapY((float) y_dim / height);
+
+  // The Escape time algorithm, it follows the pseduocode from Wikipedia
+  // _very_ closely
+  float x = 0.0;
+  float y = 0.0;
+
+  int iteration = 0;
+
+  // This can be changed, to be more or less precise
+  int max_iteration = 256;
+  while(x*x + y*y <= 4 && iteration < max_iteration) {
+    float xtemp = x*x - y*y + x_origin;
+    y = 2*x*y + y_origin;
+    x = xtemp;
+    iteration++;
+  }
+
+  if(iteration == max_iteration) {
+    // This coordinate did not escape, so it is in the Mandelbrot set
+    out[idx] = 0;
+    out[idx + 1] = 0;
+    out[idx + 2] = 0;
+  } else {
+    // This coordinate did escape, so color based on quickly it escaped
+    out[idx] = iteration;
+    out[idx + 1] = iteration;
+    out[idx + 2] = iteration;
+  }
+
 }
